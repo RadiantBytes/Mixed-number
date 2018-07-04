7c33fe471b92e7309de1241fd86534e751a35be5
RadiantBytes <radiantbytes@outlook.com>
2018-07-04 09:23:37 -0500

    GitHub upload

---
+++ Mixed Number/src/MixedNumbers.java
@@ -0,0 +1,32 @@
+
+/**
+ * This program divides two user-provided integers and converts the improper fraction into a mixed number
+ * 
+ * Last Modified: September 26 2016,
+ * @author: Thomas "RadiantBytes" Lyanugh
+ */
+import java.util.Scanner;
+
+public class MixedNumbers {
+
+	public static void main(String[] args) {
+		// Declares new scanner
+		Scanner scan = new Scanner(System.in);
+
+		// Print prompt for numerator
+		System.out.print("Enter the numerator: ");
+		int numerator = scan.nextInt();
+
+		// Print prompt for denominator
+		System.out.print("Enter the denominator: ");
+		int denominator = scan.nextInt();
+
+		// Divide numerator and denominator into a mixed number
+		String mixedNumber = (numerator / denominator) + " " + (numerator % denominator) + "/" + denominator;
+
+		// Print mixed number
+		System.out.print("## " + numerator + "/" + denominator + " = " + mixedNumber);
+
+	}
+
+}
